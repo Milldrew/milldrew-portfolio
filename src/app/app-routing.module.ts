109edb888cbbe2ceabe2eb5ec966a5d999b6b915
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-19 14:10:30 -0600

    📅  | Thu May 19 14:10:30 MDT 2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', redirectTo: '' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
