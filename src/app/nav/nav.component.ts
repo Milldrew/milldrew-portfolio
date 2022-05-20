297a4906fd32baf1f74b960740851f65889923c2
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-19 21:49:45 -0600

    📅  | Thu May 19 21:49:45 MDT 2022

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
