b7608b2430537693055a194b369e8fd714bb2737
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-19 18:50:05 -0600

    📅  | Thu May 19 18:50:05 MDT 2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'milldrew-portfolio';
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2
   ) {}
-  ngOnInit() {
-    this.renderer.setAttribute(this.document.body, 'class', 'purple-green');
-    console.log('hello');
-  }
   changeTheme(theme: string) {
     this.renderer.setAttribute(this.document.body, 'class', theme);
   }
