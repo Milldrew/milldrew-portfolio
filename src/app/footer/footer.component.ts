3b867a6d373f9350c4412a73b24623403a3eb4ba
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-25 10:20:18 -0600

    📅  | Wed May 25 10:20:18 MDT 2022

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  faGithub = faGithub;
+  faLinkedIn = faLinkedinIn;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
