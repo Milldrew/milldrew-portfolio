0e53888c36e0a0725f54479b8ed61dbd0c5dfa3f
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-27 11:33:33 -0600

    📅  | Fri May 27 11:33:33 MDT 2022

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,3 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -7,37 +6,6 @@ import { Subject, takeUntil } from 'rxjs';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnDestroy {
-  currentScreenSize: string | undefined;
-  destroyed = new Subject<void>();
-  constructor(breakpointObserver: BreakpointObserver) {
-    breakpointObserver
-      .observe([
-        Breakpoints.XSmall,
-        Breakpoints.Small,
-        Breakpoints.Medium,
-        Breakpoints.Large,
-        Breakpoints.XLarge,
-      ])
-      .pipe(takeUntil(this.destroyed))
-      .subscribe((result) => {
-        for (const query of Object.keys(result.breakpoints)) {
-          if (result.breakpoints[query]) {
-            this.currentScreenSize =
-              this.displayNameMap.get(query) ?? 'Unknown';
-          }
-        }
-      });
-  }
-
-  displayNameMap = new Map([
-    [Breakpoints.XSmall, 'XSmall'],
-    [Breakpoints.Small, 'Small'],
-    [Breakpoints.Medium, 'Medium'],
-    [Breakpoints.Large, 'Large'],
-    [Breakpoints.XLarge, 'XLarge'],
-  ]);
-  ngOnDestroy() {
-    console.log('destroyed');
-  }
+export class ProjectsComponent {
+  constructor() {}
 }
