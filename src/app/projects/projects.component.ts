5415274c2b8aa5524153e821543f7b7606356b75
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-31 10:29:25 -0600

    📅  | Tue May 31 10:29:25 MDT 2022

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ScrollToHeaderService } from '../services/scroll-to-header.service';
 import { Subject, takeUntil } from 'rxjs';
 
 @Component({
@@ -7,5 +8,9 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-  constructor() {}
+  constructor(public scrollService: ScrollToHeaderService) {}
+
+  ngAfterViewInit() {
+    this.scrollService.scrollToTop();
+  }
 }
