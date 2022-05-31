5b16728f04e63197217ce32a65738602ba41b231
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-31 10:32:05 -0600

    📅  | Tue May 31 10:32:05 MDT 2022

--- src/app/degrees/degrees.component.ts
+++ src/app/degrees/degrees.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollToHeaderService } from '../services/scroll-to-header.service';
 
 @Component({
   selector: 'app-degrees',
   templateUrl: './degrees.component.html',
-  styleUrls: ['./degrees.component.scss']
+  styleUrls: ['./degrees.component.scss'],
 })
-export class DegreesComponent implements OnInit {
+export class DegreesComponent {
+  constructor(public scrollService: ScrollToHeaderService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.scrollService.scrollToTop();
   }
-
 }
