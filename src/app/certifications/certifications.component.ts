8e947e438be9b74322e8b5fcec34bffa8a6899b8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-05-31 10:20:04 -0600

    📅  | Tue May 31 10:20:04 MDT 2022

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrollToHeaderService } from '../services/scroll-to-header.service';
 
 @Component({
   selector: 'app-certifications',
   templateUrl: './certifications.component.html',
-  styleUrls: ['./certifications.component.scss']
+  styleUrls: ['./certifications.component.scss'],
 })
 export class CertificationsComponent implements OnInit {
-
-  constructor() { }
+  constructor(public scrollService: ScrollToHeaderService) {}
 
   ngOnInit(): void {
+    this.scrollService.scrollToTop();
   }
-
 }
