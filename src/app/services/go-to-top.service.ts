dd839c0f346972b959943b0385450e4579df3f7b
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-01 20:14:03 -0600

    📅  | Wed Jun  1 20:14:03 MDT 2022

---
+++ src/app/services/go-to-top.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GoToTopService {
+  constructor() {}
+  scrollToTop(window: Window) {
+    const topElement = window.document.querySelector('#top');
+    if (topElement) {
+      topElement.scrollIntoView();
+    }
+    console.log(topElement);
+  }
+}
