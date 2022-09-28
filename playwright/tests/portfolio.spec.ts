66bafe8c0dac94307aa45dd1d79e604d32b49381
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-28 17:47:42 -0400

    📅  | Wed Sep 28 17:47:42 EDT 2022

--- playwright/tests/portfolio.spec.ts
+++ playwright/tests/portfolio.spec.ts
@@ -1,22 +1,21 @@
 const PORT_4200 = 'http://localhost:4200';
+// ==================================== SELECTORS
+const TRANSLATE_BUTTON =
+  'body > app-root > app-nav > mat-sidenav-container > mat-sidenav-content > mat-toolbar > div > button.mat-focus-indicator.language-menu.mat-icon-button.mat-button-base.mat-accent > span.mat-button-wrapper > mat-icon';
 import { test, expect } from '@playwright/test';
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({
   page,
 }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(PORT_4200);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
-  const getStarted = page.locator('text=Get Started');
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
   // Click the get started link.
-  await getStarted.click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
