6f3bcc90666a65fb0745edc3a87f4484667183b1
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-28 17:54:37 -0400

    📅  | Wed Sep 28 17:54:37 EDT 2022

--- playwright/tests/portfolio.spec.ts
+++ playwright/tests/portfolio.spec.ts
@@ -2,13 +2,16 @@ const PORT_4200 = 'http://localhost:4200';
 // ==================================== SELECTORS
 const TRANSLATE_BUTTON =
   'body > app-root > app-nav > mat-sidenav-container > mat-sidenav-content > mat-toolbar > div > button.mat-focus-indicator.language-menu.mat-icon-button.mat-button-base.mat-accent > span.mat-button-wrapper > mat-icon';
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({
   page,
 }) => {
+  const waitAndClickSelector = async (selector: string, page: Page) => {
+    await page.waitForSelector(selector);
+    await page.click(selector);
+  };
   await page.goto(PORT_4200);
-
-  const translateButton = page.locator(TRANSLATE_BUTTON);
-  await page.waitForSelector(TRANSLATE_BUTTON);
+  await waitAndClickSelector(TRANSLATE_BUTTON, page);
+  await page.waitForTimeout(60000 * 10);
 });
