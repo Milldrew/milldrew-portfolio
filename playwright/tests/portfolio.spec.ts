4c6cd6c763f45148129c75c2fccae9df2f173914
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-28 18:12:25 -0400

    📅  | Wed Sep 28 18:12:25 EDT 2022

--- playwright/tests/portfolio.spec.ts
+++ playwright/tests/portfolio.spec.ts
@@ -1,3 +1,11 @@
+const DEGREES =
+  'body > app-root > app-nav > mat-sidenav-container > mat-sidenav > div > mat-nav-list > a:nth-child(4) > span';
+const CERTIFICATIONS =
+  'body > app-root > app-nav > mat-sidenav-container > mat-sidenav > div > mat-nav-list > a:nth-child(3) > span';
+const PROJECTS =
+  'body > app-root > app-nav > mat-sidenav-container > mat-sidenav > div > mat-nav-list > a:nth-child(2) > span';
+const HOME =
+  'body > app-root > app-nav > mat-sidenav-container > mat-sidenav > div > mat-nav-list > a:nth-child(1) > span';
 const SPANISH_BUTTON =
   '#cdk-overlay-0 > mat-card > mat-action-list > button:nth-child(2) > span';
 const CHINESE_BUTTON =
@@ -17,11 +25,9 @@ test('homepage has Playwright in title and get started link linking to the intro
     await page.waitForSelector(selector);
     await page.click(selector);
   };
-  /*
   const travelEntireWebsite = async (page: Page) => {
-    page
-  }
-  */
+    page;
+  };
   await page.goto(PORT_4200);
   await waitAndClickSelector(TRANSLATE_BUTTON, page);
   await waitAndClickSelector(SPANISH_BUTTON, page);
@@ -30,4 +36,8 @@ test('homepage has Playwright in title and get started link linking to the intro
   await page.click(TRANSLATE_BUTTON);
   await waitAndClickSelector(ENGLISH_BUTTON, page);
   await page.waitForTimeout(60000 * 10);
+  await page.click(PROJECTS);
+  await page.click(CERTIFICATIONS);
+  await page.click(DEGREES);
+  await page.click(HOME);
 });
