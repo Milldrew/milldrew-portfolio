567f0dc421b237ce24c1ca13c83629d7005bdd31
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-28 18:13:46 -0400

    📅  | Wed Sep 28 18:13:46 EDT 2022

--- playwright/tests/portfolio.spec.ts
+++ playwright/tests/portfolio.spec.ts
@@ -26,7 +26,10 @@ test('homepage has Playwright in title and get started link linking to the intro
     await page.click(selector);
   };
   const travelEntireWebsite = async (page: Page) => {
-    page;
+    await page.click(PROJECTS);
+    await page.click(CERTIFICATIONS);
+    await page.click(DEGREES);
+    await page.click(HOME);
   };
   await page.goto(PORT_4200);
   await waitAndClickSelector(TRANSLATE_BUTTON, page);
@@ -35,9 +38,6 @@ test('homepage has Playwright in title and get started link linking to the intro
   await waitAndClickSelector(CHINESE_BUTTON, page);
   await page.click(TRANSLATE_BUTTON);
   await waitAndClickSelector(ENGLISH_BUTTON, page);
+  await travelEntireWebsite(page);
   await page.waitForTimeout(60000 * 10);
-  await page.click(PROJECTS);
-  await page.click(CERTIFICATIONS);
-  await page.click(DEGREES);
-  await page.click(HOME);
 });
